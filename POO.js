d7a91e22afd70abaef82db3d87e1e92d05eb2082
Fandikoos <danfandos@gmail.com>
2023-02-07 12:25:20 +0100

    POO

---
+++ POO.js
@@ -0,0 +1,17 @@
+
+
+    var miBoton=document.getElementById("boton1");      //Creamos una variable para llamar a los objetos y recogerlos en la misma
+    miBoton.style.width="300px";       //Le cambiamos sus propiedades
+    miBoton.style.height="300px";
+    //miBoton.style.backgroundColor="blue";
+
+    var miBoton2=document.getElementById("boton2");
+    miBoton2.style.width="300px";
+    miBoton2.style.height="300px";
+
+    var cuadroTexto=document.getElementById("micuadro");
+    cuadroTexto.style.background="red";
+    cuadroTexto.value="Escriba aquí...";
+    cuadroTexto.style.height="200px";
+
+    miBoton2.focus();   //Añadimos un metodo para el boton 2
